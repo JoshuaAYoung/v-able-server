fe306255b595dfbc1a2365834c8ecc4315f707c2
Josh Young <joshua@young.net>
2020-04-02 09:56:36 -0600

    mvp bug fixes

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]/
+const REGEX_UPPER_LOWER_NUMBER = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/
 
 const UsersService = {
   hasUserWithEmail(db, email) {
@@ -24,16 +24,16 @@ const UsersService = {
   },
   validatePassword(password) {
     if (password.length < 8) {
-      return 'Password must be equal to or longer than 8 characters'
+      return 'Password must be equal to or longer than 8 characters.'
     }
     if (password.length > 72) {
-      return 'Password must be less than 72 characters'
+      return 'Password must be less than 72 characters.'
     }
     if (password.startsWith(' ') || password.endsWith(' ')) {
-      return 'Password must not start or end with empty spaces'
+      return 'Password must not start or end with empty spaces.'
     }
-    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return 'Password must contain 1 upper case, lower case, number and special character'
+    if (!REGEX_UPPER_LOWER_NUMBER.test(password)) {
+      return 'Password must contain an upper case letter, a lower case letter and a number.'
     }
     return null
   },
