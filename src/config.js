5a6daf1934b255534b8dce7dee86ebf182a0e27c
Josh Young <joshua@young.net>
2020-04-04 15:10:45 -0600

    fixed server issues

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://josh_young:R4chelJ0sh11@localhost/v-able'
+  DB_URL: process.env.DB_URL || 'postgresql://josh_young:R4chelJ0sh11@localhost/v-able',
   JWT_SECRET: process.env.JWT_SECRET || 'testing-secret',
 }
