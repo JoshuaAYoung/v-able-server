8bcb84a2aa38a9f5b371bd27ff731195db97f456
Josh Young <joshua@young.net>
2020-04-10 23:35:49 -0600

    config adjustment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  e
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://josh_young:R4chelJ0sh11@localhost/v-able',
